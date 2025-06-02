24997e89a4072ed95080b2b8d6704f6b59344d56
Carla <carlamiranda3377@gmail.com>
2025-06-01 21:54:24 -0300

    Adicionei o CORS

---
+++ src/main/java/br/unifor/unimusica/config/CORSConfig.java
@@ -0,0 +1,20 @@
+package br.unifor.unimusica.config;
+
+import org.springframework.context.annotation.Configuration;
+import org.springframework.lang.NonNull;
+import org.springframework.web.servlet.config.annotation.CorsRegistry;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
+
+@Configuration
+public class CORSConfig implements WebMvcConfigurer {
+
+    @Override
+    public void addCorsMappings(@NonNull CorsRegistry registry) {
+        registry.addMapping("/**")
+                .allowedOriginPatterns("*")
+                .allowedMethods("GET", "POST", "PUT", "PATCH", "DELETE")
+                .allowedHeaders("*")
+                .allowCredentials(true);
+    }
+    
+}
